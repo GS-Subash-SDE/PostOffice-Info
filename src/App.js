2055b8664e9767cbc64d87cc439397e4daf12642
GS-Subash-SDE <gssubashofficial@gmail.com>
2024-06-22 20:12:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "material-icons/iconfont/material-icons.css";
+import {  useState } from "react";
+import SearchBox from "./components/search-box";
+import Address from "./components/address";
+import { Alert, Button, Result, Spin } from "antd";
+import Error from "./components/Error";
 
-function App() {
+const App = () => {
+  const [apiStatus, setApiStatus] = useState("init");
+  const [postoffices, setPostoffices] = useState([]);
+  const [query, setQuery] = useState('');
+  
+  document.title='Post-Office';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {(apiStatus === "init" || apiStatus === "sizeError") && (
+        <SearchBox
+          query={query}
+          setQuery={setQuery}
+          setApiStatus={setApiStatus}
+          setPostoffices={setPostoffices}
+        />
+      )}
+
+      {apiStatus === "ready" && <Address postoffices={postoffices} />}
+
+      {apiStatus === "loading" && (
+        <Spin tip="Loading...">
+          <Alert
+            message="Data Fetching"
+            description="Please wati few minutes. 🙂"
+            type="info"
+          />
+        </Spin>
+      )}
+      {apiStatus === "error" && <Error setApiStatus={setApiStatus} />}
+      {apiStatus === "sizeError" && (
+        <Result
+          status="warning"
+          title="You should enter 6 digit numbers."
+          
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
